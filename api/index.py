7de5700388f89e50e9259b4e339e2e366aadad00
unknown <rkddus222@gmail.com>
2026-01-02 15:27:30 +0900

    Fix NameError: Import Mangum in api/index.py

--- api/index.py
+++ api/index.py
@@ -9,6 +9,7 @@ parent_dir = os.path.dirname(current_dir) # 루트 디렉토리
 sys.path.append(parent_dir)
 
 from back.main import app # 실제 백엔드 앱 임포트
+from mangum import Mangum
 
 # Vercel Serverless Function 진입점
 handler = Mangum(app)
